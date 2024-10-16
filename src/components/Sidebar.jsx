226d1d4bce675eecb0ee34398213644b1d1f8e86
Fatumayattani <fyattani@gmail.com>
2024-10-16 17:08:44 +0300

    save changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,7 +11,7 @@ const Sidebar = () => {
               </div>
         <div className='flex items-center gap-3 pl-8 cursor-pointer'>
           <img className='w-6' src={assets.home_icon} alt="" />
-          <p className='font-bold'>Search</p>
+          <p className='font-bold'>Home</p>
         </div>
       </div>
       <div className='bg-[#121212] h-[85%] rounded'>
