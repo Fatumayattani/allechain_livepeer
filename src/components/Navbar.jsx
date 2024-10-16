1951408e8479656e64222a0826fe74e43aac1294
Fatumayattani <fyattani@gmail.com>
2024-10-16 13:43:25 +0300

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,6 @@ const Navbar = () => {
             <p className='bg-white text-black text-[18px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer'>Connect Wallet</p>
             <p className='bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer'>Install App</p>
             <p className='bg-pink-500 text-black w-7 h-7 rounded-full flex items-center justify-center'>A</p>
-
         </div>
 
       </div>
