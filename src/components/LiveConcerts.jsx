764fbc1f9d0225b4d33f17e761ad74d7c88396e9
Fatumayattani <fyattani@gmail.com>
2024-10-20 11:12:31 +0300

    save changes

--- src/components/LiveConcerts.jsx
+++ src/components/LiveConcerts.jsx
@@ -52,7 +52,7 @@ const LiveConcerts = () => {
 
       {/* Album List Section */}
       <div className='mb-4'>
-        <h1 className='my-5 font-bold text-2xl'>African Charts</h1>
+        <h1 className='my-5 font-bold text-2xl'>previous Concerts</h1>
         <div className='flex overflow-auto'>
           {albumsData.map((item, index) => (
             <AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />
