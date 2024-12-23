ffe13d104e962cf835febd0cb601f47558e66f60
Fatumayattani <fyattani@gmail.com>
2024-12-23 15:25:21 +0300

    save changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -122,7 +122,7 @@ const Login = () => {
 
             <button
               type="submit"
-              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               {isLogin ? 'Sign in' : 'Create Account'}
             </button>
